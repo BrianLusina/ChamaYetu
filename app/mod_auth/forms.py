1bf47a7f9789e2ea87df97bedb84121253b906c7
BrianLusina <lusinabrian@gmail.com>
2016-09-23 02:54:15 +0300

    Create LoginForm
    The Login form will be basic with inclusion of email and password.
    A class that will inherit from Form class in flask-wtf module
    Will have a StringField for email address and PasswordField for the user password

--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -1,4 +1,10 @@
 from flask_wtf import RecaptchaField, Form
-from wtforms import TextField, PasswordField, BooleanField
-from wtforms.validators import Required, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+# Define Login form
+class LoginForm(Form):
+    email = StringField("Email Address", [Email(), DataRequired(message="Forgot your email address?")])
+    password = PasswordField("Password", [DataRequired(message="Must provide a password")])
 
