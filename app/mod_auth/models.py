3d7d89528c7101a531dace778004c958416cd24c
BrianLusina <lusinabrian@gmail.com>
2016-09-24 23:39:00 +0300

    Update Model structure

--- app/mod_auth/models.py
+++ app/mod_auth/models.py
@@ -1,31 +1,37 @@
 # import database object from main app module
 from app import db
+from sqlalchemy import Column, String, ForeignKey, Integer, Float, DateTime, func, SmallInteger
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Data_Base = declarative_base()
 
 
-class Base(db.Model):
+class Base(Data_Base):
     """
     define a base model for other database tables to inherit
     """
     __abstract__ = True
 
-    id = db.Column(db.Integer, primary_key=True)
-    date_created  = db.Column(db.DateTime, default=db.func.current_timestamp())
-    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    id = Column(Integer, primary_key=True)
+    date_created = Column(DateTime, default=func.current_timestamp())
+    date_modified = Column(DateTime, default=func.current_timestamp(), onupdate=func.current_timestamp())
 
 
 class User(Base):
     __tablename__ = "user_table"
 
     # User Name
-    name = db.Column(db.String(128), nullable=False)
+    name = Column(db.String(128), nullable=False)
 
     # Identification Data: email & password
-    email = db.Column(db.String(128), nullable=False, unique=True)
-    password = db.Column(db.String(192), nullable=False)
+    email = Column(String(128), nullable=False, unique=True)
+    password = Column(String(192), nullable=False)
 
     # Authorisation Data: role & status
-    role = db.Column(db.SmallInteger, nullable=False)
-    status = db.Column(db.SmallInteger, nullable=False)
+    role = Column(SmallInteger, nullable=False)
+    status = Column(SmallInteger, nullable=False)
 
     # new instance instantiation procedure
     def __init__(self, name, email, password):
@@ -37,4 +43,6 @@ class User(Base):
         return "<User %r, Email: %r>" % (self.name, self.email)
 
 
-
+# create the database, TODO: change to postgres database
+engine = create_engine('sqlite:///app.db')
+Data_Base.metadata.create_all(engine)
