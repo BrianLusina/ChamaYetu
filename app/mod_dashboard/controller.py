022c4b0523b46181172500e8648b5ca452b0c6a2
BrianLusina <lusinabrian@gmail.com>
2016-09-25 11:15:57 +0300

    Import User Model to dashboard module

--- app/mod_dashboard/controller.py
+++ app/mod_dashboard/controller.py
@@ -1,6 +1,13 @@
 from flask import Blueprint, request, render_template, g, flash, session, redirect, url_for
+from sqlalchemy.orm import sessionmaker
+from app.models import User, Data_Base, engine
 
-# Define the blueprint: 'auth', set its url prefix: app.url/dashboard
+# Create session and connect to DB
+Data_Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+db_session = DBSession()
+
+# Define the blueprint: 'dashboard', set its url prefix: app.url/dashboard
 mod_dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 
