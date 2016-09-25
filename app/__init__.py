0c86184d14e8c1ffc0b42dd9e8849a039072cbc5
BrianLusina <lusinabrian@gmail.com>
2016-09-25 11:44:41 +0300

    Add error handlers

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,15 @@ def not_found(error):
     return render_template('404.html'), 404
 
 
+@app.errorhandler(403)
+def error_403(error):
+    return render_template("403.html"), 403
+
+
+@app.errorhandler(403)
+def error_500(error):
+    return render_template("500.html"), 500
+
 # Register blueprint(s) ALL blueprints will be registered here
 app.register_blueprint(home_module)
 app.register_blueprint(auth_module)
