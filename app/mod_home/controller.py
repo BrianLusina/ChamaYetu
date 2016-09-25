b1cd02e5c5a5d5637e09f85af0dbf49b1e7dc09f
BrianLusina <lusinabrian@gmail.com>
2016-09-25 11:13:55 +0300

    Add contact us and about pages

--- app/mod_home/controller.py
+++ app/mod_home/controller.py
@@ -5,9 +5,21 @@ mod_home = Blueprint('home', __name__, url_prefix='/')
 
 
 @mod_home.route('/')
+@mod_home.route('/index/')
 def index():
     """
     :return: Render template for home page
     """
     return render_template("home_page/index.html")
 
+
+# contact us page
+@mod_home.route('/contact/')
+def contact():
+    return render_template("home_page/contact.html")
+
+
+# about us page
+@mod_home.route('/about/')
+def contact():
+    return render_template("home_page/about.html")
