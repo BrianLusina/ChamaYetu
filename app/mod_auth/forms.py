5a5e46bc8988758872ff2305c8b25abf1e3f511f
ron93 <ronmkare1@gmail.com>
2016-09-26 12:01:08 +0300

    auth forms added registerform class

--- app/mod_auth/forms.py
+++ app/mod_auth/forms.py
@@ -8,4 +8,14 @@ class LoginForm(Form):
     email = StringField("Email Address", [Email(), DataRequired(message="Forgot your email address?")])
     password = PasswordField("Password", [DataRequired(message="Must provide a password")])
 
+#def register form
 
+class RegisterForm(Form):
+	email = StringField(
+		'email', validators =[DataRequired(),Email(message=None), Length(min = 6 ,max=40)]
+
+		)
+
+	password = PasswordField('password', validators = [DataRequired[],Length(min = 6,max=25)])
+
+	confirm = PasswordField('repeat password',validators=[DataRequired(),EqualTo('password',message='Password must match')])
