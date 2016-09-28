af655ad938330e24a86686e02b5609651692998b
ron93 <ronmkare1@gmail.com>
2016-09-28 16:46:55 +0300

    query data with time queried

--- app/mod_dashboard/controller.py
+++ app/mod_dashboard/controller.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, render_template, g, flash, session, redire
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import User, Data_Base, engine,ChamaGroup
-
+from datetime import  datetime
 
 # Create session and connect to DB
 engine = create_engine('sqlite:///app.db')
@@ -21,12 +21,14 @@ mod_dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 @mod_dashboard.route('/dashboard/')
 def dashboard():
     chamas = db_session.query(ChamaGroup).filter_by(id=1)
+    date = datetime.now()
     print(chamas)
+
     # for chama in chamas:
     #     print chama.name
     #     print  chama.total_amount
 
-    return render_template('user_dashboard/dashboard.html',chamas=chamas)
+    return render_template('user_dashboard/dashboard.html',chamas=chamas, date=date)
 
 
 
