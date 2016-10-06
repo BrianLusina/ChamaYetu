5f8fe99c6a099895adf3d3e5452f5c3c5feac950
BrianLusina <lusinabrian@gmail.com>
2016-10-06 15:29:03 +0300

    route accros blueprints

--- app/mod_dashboard/controller.py
+++ app/mod_dashboard/controller.py
@@ -2,8 +2,6 @@ from flask import Blueprint, request, render_template, \
     g, flash, session, redirect, url_for, current_app
 from sqlalchemy.orm import sessionmaker
 from app.models import User, Data_Base, engine
-from firebase import firebase
-
 
 # Create session and connect to DB
 Data_Base.metadata.bind = engine
@@ -18,4 +16,3 @@ mod_dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 def dashboard():
     firebase_base_url = current_app.config.get('FIREBASE_DB_CONN')
     return render_template('user_dashboard/dashboard.html')
-
