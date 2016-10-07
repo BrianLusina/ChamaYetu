b42aa03876ec9d7dc8ea2bca379dc1c371d30887
ron93 <ronmkare1@gmail.com>
2016-10-07 09:54:06 +0300

    dashboard queries

--- app/mod_dashboard/controller.py
+++ app/mod_dashboard/controller.py
@@ -6,6 +6,7 @@ from app.models import User, Data_Base, engine
 from firebase import firebase
 from .form import SuggProject
 import datetime
+import pprint
 
 
 
@@ -18,11 +19,33 @@ db_session = DBSession()
 mod_dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 
-@mod_dashboard.route('/')
-def dashboard():
+@mod_dashboard.route('/<username>')
+def dashboard(username):
+    """
+    :param username: the user name of the logged in user that will be appended to the url
+    Fetch the data from the chamas node
+    :return: return the dashboard template
+    """
     firebase_base_url = current_app.config.get('FIREBASE_DB_CONN')
-    return render_template('user_dashboard/dashboard.html')
+    firebase_conn = firebase.FirebaseApplication(firebase_base_url, None)
+    firebase_chama_node = current_app.config.get('FIREBASE_CHAMA_NODE')
+    firebase_statements_node = current_app.config.get('FIREBASE_STATEMENTS_NODE')
+    firebase_members_node = current_app.config.get('FIREBASE_MEMBERS_NODE')
+    firebase_users_node = current_app.config.get('FIREBASE_USERS_NODE')
 
+    # welcome our amazing user
+    flash("Welcome back " + username)
+
+    # query the user's chama (boda in this case)
+    user_chamas = firebase_conn.get(firebase_users_node+"/"+username+"/chamaGroups/boda", None)
+
+    # connect to that chama to get specific details
+    chama_details = firebase_conn.get(firebase_chama_node+"/boda", None)
+    chama_statement = firebase_conn.get(firebase_statements_node+"/boda", None)
+    pprint(chama_details)
+
+    return render_template('user_dashboard/dashboard.html', username=username,
+                           chama_details=chama_details, chama_statement=chama_statement)
 
 # global var
 count = 0
